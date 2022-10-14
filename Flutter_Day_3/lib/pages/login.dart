887fdd46cbbc4a43c4cc5e836d32570bda6d20d8
Laiba-Arooj <82440403+Laiba-Ijaz@users.noreply.github.com>
2022-10-14 19:49:13 +0500

    Day 5 Commit
    Added Single Child Scroll View and Routing

--- Flutter_Day_3/lib/pages/login.dart
+++ Flutter_Day_3/lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_day_three/utils/routes.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -7,76 +8,81 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Image.asset('assets/images/login_image.png',fit: BoxFit.cover, ),
-          ),
-          const Text('Welcome to Login Screen',
-          style: TextStyle(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image.asset('assets/images/login_image.png',fit: BoxFit.cover, ),
+            ),
+            const Text('Welcome to Login Screen',
+            style: TextStyle(
             fontWeight: FontWeight.bold,
-            fontSize: 24
-          ),),
-          const SizedBox(height: 20,),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-                child: TextFormField(
-                  obscureText: false,
-                  decoration:InputDecoration(
-                    hintText: 'example@gmail.com',
-                    labelText: 'Enter Email',
-                    prefixIcon: const Icon(Icons.email_rounded),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)
-                    )
-                  ),
-                ),
+              fontSize: 24
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration:InputDecoration(
-                      hintText: '1234567',
-                      labelText: 'Enter Password',
-                      prefixIcon: const Icon(Icons.lock_outline),
+            ),
+            const SizedBox(height: 20,),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                  child: TextFormField(
+                    obscureText: false,
+                    decoration:InputDecoration(
+                      hintText: 'example@gmail.com',
+                      labelText: 'Enter Email',
+                      prefixIcon: const Icon(Icons.email_rounded),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0)
+                        borderRadius: BorderRadius.circular(20.0)
                       )
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 60,
-                  child: ElevatedButton(
-                    onPressed: () {  },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0)
-                      ),
-                      elevation: 5,
-                      primary: Colors.deepPurpleAccent,
-                      onPrimary: Colors.white,
-                      shadowColor: Colors.deepPurpleAccent,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration:InputDecoration(
+                        hintText: '1234567',
+                        labelText: 'Enter Password',
+                        prefixIcon: const Icon(Icons.lock_outline),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0)
+                        )
                     ),
-                    child: const Text('Login'),
                   ),
-                )
-              ),
-             const Padding(
-               padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-               child: Text('Forgot Password'),
-             )
-            ],
-          )
-        ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)
+                        ),
+                        elevation: 5,
+                        primary: Colors.deepPurpleAccent,
+                        onPrimary: Colors.white,
+                        shadowColor: Colors.deepPurpleAccent,
+                      ),
+                      child: const Text('Login'),
+                    ),
+                  )
+                ),
+               const Padding(
+                 padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                 child: Text('Forgot Password'),
+               )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
