f71a98268cb8c27fc9c38baab1e220a7b9b3824b
Laiba-Arooj <82440403+Laiba-Ijaz@users.noreply.github.com>
2022-10-19 20:37:44 +0500

    Completed Day 12
    Added List Tile Displayed Items on HomePage

--- Flutter_Day_3/lib/pages/home_page.dart
+++ Flutter_Day_3/lib/pages/home_page.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../models/catalog.dart';
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog App', style: TextStyle(color: Colors.black),),
@@ -19,6 +20,9 @@ class HomePage extends StatelessWidget {
         child: ListView.builder(
           itemCount: dummyList.length,
           itemBuilder: (context,index){
+            return ItemWidget(
+              item: dummyList[index],
+            );
           }
         ),
       ),
